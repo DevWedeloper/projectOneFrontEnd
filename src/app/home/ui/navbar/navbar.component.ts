be32aef631c8ad0e41e529cfa220334a659f6c9f
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-09 23:36:37 +0800

    Made navbar component, migrated/adjusted necessary code

---
+++ src/app/home/ui/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ThemeService } from 'src/app/shared/data-access/theme.service';
+import { HomeService } from '../../data-access/home.service';
+import { SettingsDropdownComponent } from '../../features/settings-dropdown/settings-dropdown.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, SettingsDropdownComponent],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarComponent {
+  hs = inject(HomeService);
+  ts = inject(ThemeService);
+}
