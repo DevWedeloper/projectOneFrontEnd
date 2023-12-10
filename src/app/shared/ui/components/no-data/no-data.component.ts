4224598ab1b393f2ef8dd61fe2ce3db628a78f66
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-10 15:54:34 +0800

    Made no data component

---
+++ src/app/shared/ui/components/no-data/no-data.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-no-data',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './no-data.component.html',
+  styleUrls: ['./no-data.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NoDataComponent {
+
+}
