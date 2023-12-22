b0c5754b5af07893e005c7f43a045d43bf07a4fa
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-22 11:50:18 +0800

    refactor: remove unnecessary code

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,10 @@
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 export const AuthGuard = () => {
   const authService = inject(AuthService);
-  const router = inject(Router);
   if (authService.isAuthenticated()) {
     return true;
   }
@@ -14,10 +12,6 @@ export const AuthGuard = () => {
   return authService.refreshToken().pipe(
     switchMap(() => {
       return of(true);
-    }),
-    catchError(() => {
-      router.navigate(['/login']);
-      return of(false);
     })
   );
 };
