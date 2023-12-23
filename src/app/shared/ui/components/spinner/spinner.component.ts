f7ba91443810610d2c969f6b47801e7132025774
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-24 01:24:38 +0800

    refactor: prettier

--- src/app/shared/ui/components/spinner/spinner.component.ts
+++ src/app/shared/ui/components/spinner/spinner.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -7,8 +12,8 @@ import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular
   imports: [CommonModule],
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpinnerComponent {
-  @HostBinding('style.width.px') @Input({ required: true}) size!: number;
+  @HostBinding('style.width.px') @Input({ required: true }) size!: number;
 }
