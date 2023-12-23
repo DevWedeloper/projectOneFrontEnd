e04eb96439da00ac96de1f811d07f831db17a2c6
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-24 00:52:01 +0800

    refactor: rename baseUrl to url

--- src/app/guilds/data-access/check-if-member-api.service.ts
+++ src/app/guilds/data-access/check-if-member-api.service.ts
@@ -8,16 +8,16 @@ import { environment } from 'src/environments/environment';
 })
 export class CheckIfMemberApiService {
   private http = inject(HttpClient);
-  private baseUrl = environment.baseUrl;
+  private url = environment.baseUrl;
 
   isMember(character: string, guild: string): Observable<{ message: string }> {
-    const url = `${this.baseUrl}/guild/isMember`;
+    const url = `${this.url}/guild/isMember`;
     const reqBody = { character: character, guild: guild };
     return this.http.post<{ message: string }>(url, reqBody);
   }
 
   isNotMember(character: string, guild: string): Observable<{ message: string }> {
-    const url = `${this.baseUrl}/guild/isNotMember`;
+    const url = `${this.url}/guild/isNotMember`;
     const reqBody = { character: character, guild: guild };
     return this.http.post<{ message: string }>(url, reqBody);
   }
