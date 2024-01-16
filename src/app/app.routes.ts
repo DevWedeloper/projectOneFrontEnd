634089149fe221a3943ae50c3bed41c0444a711c
DevWedeloper <vicnathangabrielle@gmail.com>
2024-01-16 23:43:26 +0800

    feat: add titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Route } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginGuard } from './auth/login.guard';
 
+const mainTitle = 'ProjectOne';
+
 export const routes: Route[] = [
   {
     path: 'login',
     loadComponent: () =>
       import('./login/login.component').then((m) => m.LoginComponent),
     canActivate: [LoginGuard],
+    title: `${mainTitle} | Login`
   },
   {
     path: '',
@@ -26,6 +29,7 @@ export const routes: Route[] = [
           import('./dashboard/dashboard.component').then(
             (m) => m.DashboardComponent,
           ),
+        title: `${mainTitle} | Dashboard`
       },
       {
         path: 'characters',
@@ -33,11 +37,13 @@ export const routes: Route[] = [
           import('./characters/characters.component').then(
             (m) => m.CharactersComponent,
           ),
+        title: `${mainTitle} | Characters`
       },
       {
         path: 'guilds',
         loadComponent: () =>
           import('./guilds/guilds.component').then((m) => m.GuildsComponent),
+        title: `${mainTitle} | Guilds`
       },
     ],
   },
