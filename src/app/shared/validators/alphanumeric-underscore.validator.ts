0e1a7d8f8bea0e7662b3f84dbf273eda4c0585f9
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-01 05:41:58 +0800

    fix: validator not working properly

--- src/app/shared/validators/alphanumeric-underscore.validator.ts
+++ src/app/shared/validators/alphanumeric-underscore.validator.ts
@@ -1,10 +1,10 @@
-import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
-export function alphanumericUnderscore(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const pattern = /^[a-zA-Z0-9_]+$/;
-    const isValid = pattern.test(control.value);
+export function alphanumericUnderscore(
+  control: AbstractControl,
+): ValidationErrors | null {
+  const pattern = /^[a-zA-Z0-9_]+$/;
+  const isValid = pattern.test(control.value);
 
-    return isValid ? null : { alphanumericUnderscore: true };
-  };
+  return isValid ? null : { alphanumericUnderscore: true };
 }
