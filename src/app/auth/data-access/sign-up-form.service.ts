8e5320fe5ad1de6c7f0bf83a0b88105823080fa9
DevWedeloper <vicnathangabrielle@gmail.com>
2024-01-31 09:28:09 +0800

    refactor: use shared async validators

--- src/app/auth/data-access/sign-up-form.service.ts
+++ src/app/auth/data-access/sign-up-form.service.ts
@@ -1,22 +1,17 @@
 import { Injectable, inject } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  ValidationErrors,
-  Validators,
-} from '@angular/forms';
-import { Observable, catchError, debounceTime, map, of, switchMap } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { customPassword } from '../validators/custom-password.validator';
 import { passwordShouldMatch } from '../validators/password-should-match.validator';
-import { UserApiService } from './user-api.service';
+import { UniqueEmailValidator } from '../validators/unique-email.validator';
+import { UniqueUsernameValidator } from '../validators/unique-username.validator';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SignUpFormService {
   private fb = inject(FormBuilder);
-  private userApiService = inject(UserApiService);
+  private uniqueEmail = inject(UniqueEmailValidator);
+  private uniqueUsername = inject(UniqueUsernameValidator);
 
   initializeSignupForm(): FormGroup {
     return this.fb.group(
@@ -25,7 +20,7 @@ export class SignUpFormService {
           '',
           {
             validators: [Validators.required, Validators.email],
-            asyncValidators: [this.uniqueEmail.bind(this)],
+            asyncValidators: [this.uniqueEmail.validate.bind(this.uniqueEmail)],
           },
         ],
         username: [
@@ -37,7 +32,9 @@ export class SignUpFormService {
               Validators.maxLength(20),
               Validators.pattern(/^[a-zA-Z0-9_]+$/),
             ],
-            asyncValidators: [this.uniqueUsername.bind(this)],
+            asyncValidators: [
+              this.uniqueUsername.validate.bind(this.uniqueUsername),
+            ],
           },
         ],
         password: ['', [Validators.required, customPassword]],
@@ -47,52 +44,4 @@ export class SignUpFormService {
       { validators: passwordShouldMatch },
     );
   }
-
-  private uniqueEmail = (
-    control: AbstractControl,
-  ): Observable<ValidationErrors | null> => {
-    if (control.getRawValue() === '') {
-      return of(null);
-    }
-
-    return of(control.value).pipe(
-      debounceTime(500),
-      switchMap((email) =>
-        this.userApiService.isEmailUnique(email).pipe(
-          map((response) =>
-            response.message === 'Email is unique'
-              ? null
-              : { uniqueName: true },
-          ),
-          catchError(() => {
-            return of(null);
-          }),
-        ),
-      ),
-    );
-  };
-
-  private uniqueUsername = (
-    control: AbstractControl,
-  ): Observable<ValidationErrors | null> => {
-    if (control.getRawValue() === '') {
-      return of(null);
-    }
-
-    return of(control.value).pipe(
-      debounceTime(500),
-      switchMap((username) =>
-        this.userApiService.isUsernameUnique(username).pipe(
-          map((response) =>
-            response.message === 'Username is unique'
-              ? null
-              : { uniqueName: true },
-          ),
-          catchError(() => {
-            return of(null);
-          }),
-        ),
-      ),
-    );
-  };
 }
